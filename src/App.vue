42c4fe2eac91a1d99a37b2524645e45bf4ba1eef
danielv <daniel@eliyot.com>
2021-04-13 20:29:16 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<h1>Vue array type</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+interface MyArray extends Array<number> {};
+
+export default defineComponent({
+  name: 'App',
+	
+	emits: ["test"],
+	
+	data() {
+	},
+
+	mounted() {
+		this.$nextTick(() => {
+
+		});
+	}
+
+});
+</script>
+
+
